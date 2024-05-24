a215cf26dd4267841962799334443167a437c188
daniOs110 <daniOs2010@gmail.com>
2024-05-24 01:33:07 -0600

    cors update

--- src/app/app.js
+++ src/app/app.js
@@ -28,7 +28,7 @@ const app = express()
 
 app.use(morgan('dev'))
 app.use(express.json())
-app.use(cors(corsOptions))
+// app.use(cors(corsOptions))
 
 app.get('/', (req, res) => {
   res.send('This is express')
