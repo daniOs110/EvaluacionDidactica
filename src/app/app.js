e7061b5cf6810392f41565c494314081b77e3b4b
daniOs110 <daniOs2010@gmail.com>
2024-05-24 02:51:57 -0600

    edit app class

--- src/app/app.js
+++ src/app/app.js
@@ -42,7 +42,7 @@ const app = express()
 app.use(morgan('dev'))
 app.use(express.json())
 // app.use(cors(corsOptions))
-app.use(cors())
+// app.use(cors())
 // app.use(cors(corsOptions))
 app.use(cors());
 
