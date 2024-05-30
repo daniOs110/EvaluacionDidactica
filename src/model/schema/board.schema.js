ff71fd341a8ba1b3cfd6854b8fe406d14317eec1
daniOs110 <daniOs2010@gmail.com>
2024-05-30 02:25:19 -0600

    fix models route sequelize

--- src/model/schema/board.schema.js
+++ src/model/schema/board.schema.js
@@ -1,5 +1,5 @@
 const { Sequelize, Model, DataTypes } = require('sequelize')
-const sequelize = require('../config/database')
+const sequelize = require('../../config/database')
 const Evaluacion = require('../schema/evaluation.schemas')
 
 class Tablero extends Model {}
--- src/model/schema/positionCrosswords.schema.js
+++ src/model/schema/positionCrosswords.schema.js
@@ -1,5 +1,5 @@
 const { Sequelize, Model, DataTypes } = require('sequelize')
-const sequelize = require('../config/database')
+const sequelize = require('../../config/database')
 const Pregunta = require('../schema/questions.schema')
 
 class PosicionCrucigrama extends Model {}
